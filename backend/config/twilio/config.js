b2700fc498c33904c126a78d0ed871e0a066c92d
Jefferson Benedito <jefiim.ad@gmail.com>
2018-12-13 15:06:44 -0300

    twilio's config

---
+++ backend/config/twilio/config.js
@@ -0,0 +1,9 @@
+(function () {
+
+    var config = {
+        accountSid: 'AC7195997d2ec6a41709ffa11cf91dcfe4',
+        authToken: 'e9e2ac0fa1e46d2db8a6e694c0aa15ee'
+    };
+
+    module.exports = config;
+}());
